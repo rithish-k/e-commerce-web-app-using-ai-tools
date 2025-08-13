cede3e07405f2c9483212851f40f06fb0cb4166d
rithish-k <rithishgajjala@gmail.com>
2025-08-12 22:25:20 -0400

    backend  working at localhost 3002 not at 3001

--- backend/index.js
+++ backend/index.js
@@ -32,6 +32,6 @@ app.post('/register',(req,res)=>{
     .then(signupinfo =>res.json(signupinfo))
     .catch(err=>res.status(500).json(err))
 })
-app.listen(3001,()=>{ 
+app.listen(3002,()=>{ 
   console.log("server is running")
 })
--- backend/models/signup.js
+++ backend/models/signup.js
@@ -1,9 +1,9 @@
 const mongoose= require("mongoose")
 
 const signupSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String   
+    name: String,
+    email: String,
+    password: String   
 })
 const signupModel = mongoose.model("signupinfo",signupSchema)
 module.exports = signupModel
